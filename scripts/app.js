95dc4d1b34222fa1f0f04b8dab9f313cbcd13b5e
Chottcodes <cwellshott@gmail.com>
2024-12-09 18:43:27 -0800

    update to api call

--- scripts/app.js
+++ scripts/app.js
@@ -2,10 +2,13 @@ import { apikey } from "./enviroment.js";
 
 async function retrieveData() {
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=37.961632&lon=-121.275604&appid=${apikey}`
+    `https://api.openweathermap.org/data/2.5/weather?lat=37.961632&lon=-121.275604&appid=${apikey}&units=imperial`
   );
-  const data=response.json();
-  console.log(data);
+  const data=await response.json();
+  console.log(`The current temp in stockton is ${data.main.temp}°F`);
+  console.log(`The High: ${data.main.temp_max}°F`);
+  console.log(`The Low: ${data.main.temp_min}°F`);
+  
 
 }
 retrieveData();
